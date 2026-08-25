af44d1c5209c25b07322fd0348a59c605ffe4d06
renanfertrin <renanfertrin@gmail.com>
2026-08-24 21:34:32 -0300

    Ativa recursos v5 no aplicativo

--- sw.js
+++ sw.js
@@ -1,4 +1,16 @@
 const CACHE='meu-treino-v5';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./workout-v5.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'])))});
+const ASSETS=['./','./index.html','./workout-v5.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin)e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)))})
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET'||new URL(e.request.url).origin!==location.origin)return;
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch('./index.html',{cache:'no-store'}).then(async r=>{
+      let html=await r.text();
+      if(!html.includes('workout-v5.js'))html=html.replace('</body>','<script src="./workout-v5.js?v=5"></script></body>');
+      return new Response(html,{status:200,headers:{'Content-Type':'text/html; charset=utf-8','Cache-Control':'no-store'}});
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)));
+});
